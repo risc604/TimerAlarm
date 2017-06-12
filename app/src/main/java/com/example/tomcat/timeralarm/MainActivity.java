dafbeb7036cd13dc977221c3f9f1ac02eb3e6cd1
tomcat <tomcat.cheng@microlife.com.tw>
2017-06-12 15:44:07 +0800

    handler test

--- app/src/main/java/com/example/tomcat/timeralarm/MainActivity.java
+++ app/src/main/java/com/example/tomcat/timeralarm/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.tomcat.timeralarm;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -11,6 +13,8 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity
 {
+    private final static String TAG = MainActivity.class.getSimpleName();
+
     Handler     handler;
     //Timer       notifyTimer;
     TextView    textView;
@@ -26,36 +30,52 @@ public class MainActivity extends AppCompatActivity
         initControl();
     }
 
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+    }
 
-
+    @Override
+    protected void onDestroy()
+    {
+        handler.removeCallbacks(runnable);
+        super.onDestroy();
+    }
 
     private void initView()
     {
         textView = (TextView) findViewById(R.id.tvMesg);
+        textView.setTextSize(20);
+        //textView.set
     }
 
     private void initControl()
     {
         handler = new Handler();
-        }
-
-        runnable  = new Runnable() {
+        runnable  = new Runnable()
+        {
             @Override
             public void run() {
                 showTime();
             }
         };
 
+        //showTime();
+        //handler.postDelayed(runnable, 1000);
+        handler.post(runnable);
+        //runnable.run();
 
     }
 
 
     public void showTime()
     {
-            Date nowDt = Calendar.getInstance().getTime();
-            SimpleDateFormat sft = new SimpleDateFormat("yyyy/MM/DD hh:mm:ss");
-            String msg = sft.format(nowDt);
-            textView.setText(msg);
+        Date nowDt = Calendar.getInstance().getTime();
+        SimpleDateFormat sft = new SimpleDateFormat("yyyy/MM/dd   HH:mm:ss");
+        String msg = sft.format(nowDt);
+        textView.setText(msg);
+        handler.postDelayed(runnable, 1000*10);
+        Log.d(TAG, msg);
     }
-
 }
