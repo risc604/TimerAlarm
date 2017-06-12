4c7cdded06c34f5d8d4b91e1a45c48cde5ff57dd
tomcat <risc604@ms40.url.com.tw>
2017-06-12 14:36:27 +0800

    add code.

--- app/src/main/java/com/example/tomcat/timeralarm/MainActivity.java
+++ app/src/main/java/com/example/tomcat/timeralarm/MainActivity.java
@@ -2,12 +2,60 @@ package com.example.tomcat.timeralarm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
+
+
+public class MainActivity extends AppCompatActivity
+{
+    Handler     handler;
+    //Timer       notifyTimer;
+    TextView    textView;
+    Runnable    runnable;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initControl();
     }
+
+
+
+
+    private void initView()
+    {
+        textView = (TextView) findViewById(R.id.tvMesg);
+    }
+
+    private void initControl()
+    {
+        handler = new Handler();
+        }
+
+        runnable  = new Runnable() {
+            @Override
+            public void run() {
+                showTime();
+            }
+        };
+
+
+    }
+
+
+    public void showTime()
+    {
+            Date nowDt = Calendar.getInstance().getTime();
+            SimpleDateFormat sft = new SimpleDateFormat("yyyy/MM/DD hh:mm:ss");
+            String msg = sft.format(nowDt);
+            textView.setText(msg);
+    }
+
 }
